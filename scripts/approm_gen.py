fe7f18db1d252f4977cc4206d2af5243224d0462
TendingStream73 <sasasabaev679@gmail.com>
2024-06-13 22:11:04 +0300

    scripts: update approm_gen.py

--- scripts/approm_gen.py
+++ scripts/approm_gen.py
@@ -1,5 +1,7 @@
 from ctypes import BigEndianStructure, c_uint32, sizeof
 import argparse
+from struct import unpack
+from os import SEEK_END
 
 parser = argparse.ArgumentParser()
 parser.add_argument("INPUT", help="Approm that needs to be fixed")
@@ -32,3 +34,53 @@ class AppRomHeader(BigEndianStructure):
     _pack_ = 0
 
 inp = open(args.INPUT, 'rb') # open input
+
+hdr_data = bytearray(inp.read(sizeof(AppRomHeader)))
+inp.seek(0)
+
+hdr = AppRomHeader.from_buffer(hdr_data)
+
+l = hdr.rom_length - hdr.checksum
+code_len = (l / 4) + 3
+
+joeb = 0
+while (code_len-int(code_len)) != 0.0:
+    joeb += 1
+    l += 1
+    code_len = (l / 4) + 3
+
+del l
+
+inp.seek(0, SEEK_END)
+size = inp.tell()
+inp.seek(0)
+
+hdr.code_length = int(code_len)
+
+out = open(args.OUTPUT, "wb+") # open output
+out.write(hdr_data)
+inp.seek(0x44)
+
+out.write(inp.read())
+
+rfs = open(args.romfs, 'rb')
+out.write(rfs.read())
+rfs.close()
+inp.close()
+
+rl = out.tell()
+hdr.rom_length = int((rl / 4) + 3)
+out.seek(0)
+out.write(hdr_data)
+
+out.seek(0)
+checksum = unpack(">I", out.read(4))[0]
+out.seek(0xc)
+l = (code_len - 3) * 4
+l -= 0xc
+for i in range(round(l / 4)):
+    checksum += unpack(">I", out.read(4))[0]
+
+hdr.checksum = checksum & 0xffffffff
+out.seek(0)
+out.write(hdr_data)
